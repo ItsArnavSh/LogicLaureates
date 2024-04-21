9b36a7cd5941d4dfae3f442b7da125636d482d60
itsarnavsh <its.arnav.sh@gmail.com>
2024-04-21 13:35:04 +0530

    Pending

--- frontend/src/routes/storage-owner-dashboard/+page.svelte
+++ frontend/src/routes/storage-owner-dashboard/+page.svelte
@@ -21,13 +21,13 @@
         <h1 class="font-bold text-3xl font-mono">Dashboard</h1>
         <a href="/apply"><button class="px-6 py-4 bg-black text-white rounded-md hover:scale-95 hover:bg-white hover:text-black transition-all duration-200 shadow-md">Create</button></a>
         <h2>Pending for review</h2>
-        <div class="flex w-[500px] justify-evenly">
+        <div class="flex w-[80vw] justify-evenly ">
             {#each pendingLands as pl ,i}
-                <div class=" border-2 border-black shadow-md hover:scale-95">
-                    <div>
-                        <h1>{pl.propertyName}</h1>
-                        <p>{pl.description}</p>
-                        <p>Monthly Price: {pl.monthlyPrice}</p>
+                <div class="  border-black border-4 m-2 shadow-md hover:scale-95 w-[20vw]">
+                    <div class = "flex flex-col items-center p-4">
+                        <h1 class = "text-xl"><b>{pl.propertyName}</b></h1>
+                        <p class = "text-xl">{pl.description}</p>
+                        <p class = "text-xl">Monthly Price: {pl.monthlyPrice}</p>
                         <div class="flex justify-between w-full">
                             <button>Edit</button>
                             <button>Delete</button>
