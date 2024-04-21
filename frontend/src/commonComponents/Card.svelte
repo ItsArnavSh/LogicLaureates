e298b997478ce38b7ed2b4ecc968ceacb3a78cdf
itsarnavsh <its.arnav.sh@gmail.com>
2024-04-21 12:59:03 +0530

    Another One

---
+++ frontend/src/commonComponents/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let thumbnail;
+    export let area;
+    export let price;
+    export let city;
+
+</script>
+<div class = "w-[20vw] p-5 border-black border-8">
+    <img src = {thumbnail} class = "">
+    <div class = "flex flex-col items-center">
+        <p>Area:{area} sq m</p>
+        <p>Price:{price}</p>
+        <p>City:{city} sq m</p>
+    </div>
+</div>
